ef957b6a80af87fa775ba63080084371b69d6bdb
congnhat234 <31690712+congnhat234@users.noreply.github.com>
2018-03-15 01:33:51 +0700

    Change

--- app/src/main/java/com/smartparking/admin/MainActivity.java
+++ app/src/main/java/com/smartparking/admin/MainActivity.java
@@ -8,7 +8,9 @@ import android.text.Html;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,51 +20,66 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView lvData;
+    ListView lvData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvData = (ListView) findViewById(R.id.lvData);
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
 //        DatabaseReference myFirebaseRef = database.getReference();
-        DatabaseReference myRef = database.getReference("Devpro");
+        DatabaseReference myRef = database.getReference();
 //
 //        myRef.setValue("Hello, World!");
 //
-//        for (int i = 0; i <= 5; i++) {
-//            myFirebaseRef.child("Devpro").child("key " + i).setValue("Value " + i);
-//        }
+        for (int i = 0; i <= 5; i++) {
+            myRef.child("Devpro").child("key " + i).setValue("Value " + i);
+        }
 
 //        Firebase.setAndroidContext(this);
-        lvData = (ListView) findViewById(R.id.lvData);
+
 
 //        Firebase myFirebaseRef = new Firebase("https://devpro-hello.firebaseio.com/");
 //        for (int i = 0; i <= 10; i++) {
-//            myFirebaseRef.child("Devpro").child("key " + i).setValue("Value " + i);
+//            myRef.child("Devpro").child("key " + i).setValue("Value " + i);
 //        }
 
         // Cấu hình ListView
         final ArrayList<String> name = new ArrayList<String>();
-        final ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1);
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, name);
         lvData.setAdapter(adapter);
 
         // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef.child("Devpro").addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                // value = dataSnapshot.getValue(String.class);
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 name.add(dataSnapshot.getValue().toString());
+
                 adapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                //Log.w(Tag, "Failed to read value.", error.toException());
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                name.add(dataSnapshot.getValue().toString());
+                name.add("test");
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
